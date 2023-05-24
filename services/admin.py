1ad6c3cb26c88095fde564863805811744c5f0fd
Michael-Zapivahin <zapivahin@ya.ru>
2023-05-24 09:56:44 +0300

    add models

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Client, Employee, Procedure
+
 
 # Register your models here.
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Procedure)
+class ProcedureAdmin(admin.ModelAdmin):
+    pass
--- services/models.py
+++ services/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+# from tinymce.models import HTMLField
 
-# Create your models here.
+
+class Client(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Name')
+    phone = models.CharField(max_length=12, verbose_name='Phone')
+
+    def __str__(self):
+        return f'{self.Name}, phone: {self.phone}'
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Name')
+    phone = models.CharField(max_length=12, verbose_name='Phone')
+    photo = models.ImageField(max_length=200, verbose_name='Photo', blank=True, null=True)
+    position = models.CharField(max_length=200, verbose_name='Position')
+
+    def __str__(self):
+        return f'{self.name} pos: {self.position} ph: {self.phone}'
+
+
+class Procedure(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Name')
+    cost = models.IntegerField(verbose_name='Cost')
+    time = models.IntegerField(verbose_name='Time in minutes', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
