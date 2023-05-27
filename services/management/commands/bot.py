913998dc9482b1216ffa55b29c72384f175ded2f
Michael-Zapivahin <zapivahin@ya.ru>
2023-05-27 16:16:29 +0300

    del test bot

--- services/management/commands/bot.py
+++ services/management/commands/bot.py
@@ -27,10 +27,6 @@ IMAGES_URL = os.environ['IMAGES_URL']
 token = TG_TOKEN
 provider_token = PAYMENTS_TOKEN
 bot = telebot.TeleBot(token)
-prices = [LabeledPrice(label='Working Time Machine', amount=5750), LabeledPrice('Gift wrapping', 500)]
-shipping_options = [
-    ShippingOption(id='instant', title='WorldWide Teleporter').add_price(LabeledPrice('Teleporter', 1000)),
-    ShippingOption(id='pickup', title='Local pickup').add_price(LabeledPrice('Pickup', 300))]
 
 
 def get_calendar(call_back, month=None):
@@ -190,68 +186,6 @@ class BOT:
 
         bot = telebot.TeleBot(TG_TOKEN)
 
-        # start payment block
-
-        @bot.message_handler(commands=['start_payment'])
-        def command_start(message):
-            bot.send_message(message.chat.id,
-                             "Hello, I'm the demo merchant bot."
-                             " I can sell you a Time Machine."
-                             " Use /buy to order one, /terms for Terms and Conditions")
-
-        @bot.message_handler(commands=['terms'])
-        def command_terms(message):
-            bot.send_message(message.chat.id,
-                             'Thank you for shopping with our demo bot. We hope you like your new time machine!\n'
-                             '1. If your time machine was not delivered on time, please rethink your concept of time and try again.\n'
-                             '2. If you find that your time machine is not working, kindly contact our future service workshops on Trappist-1e.'
-                             ' They will be accessible anywhere between May 2075 and November 4000 C.E.\n'
-                             '3. If you would like a refund, kindly apply for one yesterday and we will have sent it to you immediately.')
-
-        @bot.message_handler(commands=['buy'])
-        def command_pay(message):
-            bot.send_message(message.chat.id,
-                             "Real cards won't work with me, no money will be debited from your account."
-                             " Use this test card number to pay for your Time Machine: `4242 4242 4242 4242`"
-                             "\n\nThis is your demo invoice:", parse_mode='Markdown')
-            bot.send_invoice(
-                message.chat.id,  # chat_id
-                'Working Time Machine',  # title
-                ' Want to visit your great-great-great-grandparents? Make a fortune at the races? Shake hands with Hammurabi and take a stroll in the Hanging Gardens? Order our Working Time Machine today!',
-                # description
-                'HAPPY FRIDAYS COUPON',  # invoice_payload
-                provider_token,  # provider_token
-                'usd',  # currency
-                prices,  # prices
-                photo_url='http://erkelzaar.tsudao.com/models/perrotta/TIME_MACHINE.jpg',
-                photo_height=512,  # !=0/None or picture won't be shown
-                photo_width=512,
-                photo_size=512,
-                is_flexible=False,  # True If you need to set up Shipping Fee
-                start_parameter='time-machine-example')
-
-        @bot.shipping_query_handler(func=lambda query: True)
-        def shipping(shipping_query):
-            print(shipping_query)
-            bot.answer_shipping_query(shipping_query.id, ok=True, shipping_options=shipping_options,
-                                      error_message='Oh, seems like our Dog couriers are having a lunch right now. Try again later!')
-
-        @bot.pre_checkout_query_handler(func=lambda query: True)
-        def checkout(pre_checkout_query):
-            bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True,
-                                          error_message="Aliens tried to steal your card's CVV, but we successfully protected your credentials,"
-                                                        " try to pay again in a few minutes, we need a small rest.")
-
-        @bot.message_handler(content_types=['successful_payment'])
-        def got_payment(message):
-            bot.send_message(message.chat.id,
-                             'Hoooooray! Thanks for payment! We will proceed your order for `{} {}` as fast as possible! '
-                             'Stay in touch.\n\nUse /buy again to get a Time Machine for your friend!'.format(
-                                 message.successful_payment.total_amount / 100, message.successful_payment.currency),
-                             parse_mode='Markdown')
-
-        # end payment block
-
         @bot.message_handler(commands=['start', 'help'])
         def start(message):
             # Отправка стартового сообщения
@@ -282,13 +216,35 @@ class BOT:
         @bot.callback_query_handler(func=lambda call: True)
         def handle_callback(call):
 
+            if call.data.startswith('start_payment_buy'):
+                amount = 100
+                procedure = 'hair dressed'
+                bot.send_invoice(
+                    call.message.chat.id,  # chat_id
+                    'Оплата услуг салона',  # title
+                    'Вы можете оплатить услуги на месте!',
+                    # description
+                    'HAPPY FRIDAYS COUPON',  # invoice_payload
+                    provider_token,
+                    'rub',
+                    prices=[LabeledPrice(
+                        label='Услуги салона...', amount=amount
+                    ), LabeledPrice(procedure, amount*100)],
+                    photo_url='',
+                    photo_height=512,  # !=0/None or picture won't be shown
+                    photo_width=512,
+                    photo_size=512,
+                    is_flexible=False,  # True If you need to set up Shipping Fee
+                    start_parameter='service-example')
+
             if call.data == 'record':
                 global RECORD_INF
                 RECORD_INF = {}
                 keyboard = types.InlineKeyboardMarkup()
                 button_master = types.InlineKeyboardButton(text='Выбрать мастера', callback_data='select_master')
                 button_salon = types.InlineKeyboardButton(text='Выбор салона', callback_data='select_salon')
-                keyboard.add(button_master, button_salon)
+                button_pyment = types.InlineKeyboardButton(text='Оплатить', callback_data='start_payment_buy')
+                keyboard.add(button_master, button_salon, button_pyment)
                 bot.edit_message_text(
                     chat_id=call.message.chat.id, message_id=call.message.message_id,
                     text='Супер, начнем запись!', reply_markup=keyboard
@@ -412,19 +368,9 @@ class BOT:
                     reply_markup=markup
                 )
 
-            if call.data.startswith('payment'):
-                markup = types.InlineKeyboardMarkup()
-                markup.row(types.InlineKeyboardButton('Back', callback_data='time'))
-                bot.send_message(
-                    call.message.chat.id,
-                    'payment start',
-                    reply_markup=markup
-                )
-
             if call.data.startswith('time'):
                 markup = types.InlineKeyboardMarkup()
                 markup.row(types.InlineKeyboardButton('Назад', callback_data='day'))
-                markup.row(types.InlineKeyboardButton('Payment', callback_data='payment'))
                 RECORD_INF['time'] = call.data
                 bot.send_message(
                     call.message.chat.id,
@@ -464,6 +410,26 @@ class BOT:
             bot.send_message(message.chat.id, f"Ваша запись {RECORD_INF} \n до встречи")
             global is_name_registered
             is_name_registered = False
+
+        # start payment block
+
+        @bot.shipping_query_handler(func=lambda query: True)
+        def shipping(shipping_query):
+            pass
+
+        @bot.pre_checkout_query_handler(func=lambda query: True)
+        def checkout(pre_checkout_query):
+            bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True,
+                                          error_message="Произошла ошибка при оплате, попробуйте еще раз.")
+
+        @bot.message_handler(content_types=['successful_payment'])
+        def got_payment(message):
+            bot.send_message(message.chat.id,
+                             'Срасибо за платеж! Мы будем рады видеть вас в нашем салоне! '.format(
+                                 message.successful_payment.total_amount / 100, message.successful_payment.currency),
+                             parse_mode='Markdown')
+
+        # end payment block
 
         bot.polling()
 
