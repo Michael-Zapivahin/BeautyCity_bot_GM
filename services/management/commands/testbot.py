7b0dfe5d8ae1cd8b6c5919eedaa02c38ec6ade2c
Michael-Zapivahin <zapivahin@ya.ru>
2023-05-27 15:39:08 +0300

    make payment test sberbank

--- services/management/commands/testbot.py
+++ services/management/commands/testbot.py
@@ -27,10 +27,6 @@ IMAGES_URL = os.environ['IMAGES_URL']
 token = TG_TOKEN
 provider_token = PAYMENTS_TOKEN
 bot = telebot.TeleBot(token)
-prices = [LabeledPrice(label='Working Time Machine', amount=5750), LabeledPrice('Gift wrapping', 500)]
-shipping_options = [
-    ShippingOption(id='instant', title='WorldWide Teleporter').add_price(LabeledPrice('Teleporter', 1000)),
-    ShippingOption(id='pickup', title='Local pickup').add_price(LabeledPrice('Pickup', 300))]
 
 
 def get_calendar(call_back, month=None):
@@ -221,25 +217,25 @@ class BOT:
         def handle_callback(call):
 
             if call.data.startswith('start_payment_buy'):
-                bot.send_message(call.message.chat.id,
-                                 "Real cards won't work with me, no money will be debited from your account."
-                                 " Use this test card number to pay for your Time Machine: `4242 4242 4242 4242`"
-                                 "\n\nThis is your demo invoice:", parse_mode='Markdown')
+                amount = 100
+                procedure = 'hair dressed'
                 bot.send_invoice(
                     call.message.chat.id,  # chat_id
-                    'Working Time Machine',  # title
-                    ' Want to visit your great-great-great-grandparents? Make a fortune at the races? Shake hands with Hammurabi and take a stroll in the Hanging Gardens? Order our Working Time Machine today!',
+                    'Оплата услуг салона',  # title
+                    'Вы можете оплатить услуги на месте!',
                     # description
                     'HAPPY FRIDAYS COUPON',  # invoice_payload
-                    provider_token,  # provider_token
-                    'usd',  # currency
-                    prices,  # prices
-                    photo_url='http://erkelzaar.tsudao.com/models/perrotta/TIME_MACHINE.jpg',
+                    provider_token,
+                    'rub',
+                    prices=[LabeledPrice(
+                        label='Услуги салона...', amount=amount
+                    ), LabeledPrice(procedure, amount*100)],
+                    photo_url='',
                     photo_height=512,  # !=0/None or picture won't be shown
                     photo_width=512,
                     photo_size=512,
                     is_flexible=False,  # True If you need to set up Shipping Fee
-                    start_parameter='time-machine-example')
+                    start_parameter='service-example')
 
             if call.data == 'record':
                 global RECORD_INF
@@ -417,41 +413,9 @@ class BOT:
 
         # start payment block
 
-        # @bot.message_handler(func=lambda message: True)
-        # def command_start(message):
-        #     if message.text == 'start_payment':
-        #         bot.send_message(message.chat.id,
-        #                          "Hello, I'm the demo merchant bot."
-        #                          " I can sell you a Time Machine."
-        #                          " Use /buy to order one.")
-
-        @bot.message_handler(commands=['buy'])
-        def command_pay(message):
-            bot.send_message(message.chat.id,
-                             "Real cards won't work with me, no money will be debited from your account."
-                             " Use this test card number to pay for your Time Machine: `4242 4242 4242 4242`"
-                             "\n\nThis is your demo invoice:", parse_mode='Markdown')
-            bot.send_invoice(
-                message.chat.id,  # chat_id
-                'Working Time Machine',  # title
-                ' Want to visit your great-great-great-grandparents? Make a fortune at the races? Shake hands with Hammurabi and take a stroll in the Hanging Gardens? Order our Working Time Machine today!',
-                # description
-                'HAPPY FRIDAYS COUPON',  # invoice_payload
-                provider_token,  # provider_token
-                'usd',  # currency
-                prices,  # prices
-                photo_url='http://erkelzaar.tsudao.com/models/perrotta/TIME_MACHINE.jpg',
-                photo_height=512,  # !=0/None or picture won't be shown
-                photo_width=512,
-                photo_size=512,
-                is_flexible=False,  # True If you need to set up Shipping Fee
-                start_parameter='time-machine-example')
-
         @bot.shipping_query_handler(func=lambda query: True)
         def shipping(shipping_query):
-            print(shipping_query)
-            bot.answer_shipping_query(shipping_query.id, ok=True, shipping_options=shipping_options,
-                                      error_message='Oh, seems like our Dog couriers are having a lunch right now. Try again later!')
+            pass
 
         @bot.pre_checkout_query_handler(func=lambda query: True)
         def checkout(pre_checkout_query):
